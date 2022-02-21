56e1615e00c965e0f369cb9ecb0faafaacd2d513
emanuel <manii.romeroo@gmail.com>
2022-02-21 12:33:22 -0600

    not found pague example

--- app/index.js
+++ app/index.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
-app.listen(3000)
+app.get("/hola-mundo", function (req, res) {
+  res.send("Hello World");
+});
+
+/* Si no encuentra ninguna ruta */
+app.get("*", function (req, res) {
+  res.send("404 not found");
+});
+
+app.listen(3000);
